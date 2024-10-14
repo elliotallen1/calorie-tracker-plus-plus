07867c85fff92b5fd2dc09237c1d0f6e1bb92473
Amelia Jones <jonesas@hendrix.edu>
2024-10-14 17:58:03 -0500

    Removed print statements

--- calorietrackerplusplus/lib/src/authentication.dart
+++ calorietrackerplusplus/lib/src/authentication.dart
@@ -25,7 +25,6 @@ class AuthFunc extends StatelessWidget {
           padding: const EdgeInsets.only(left: 24, bottom: 8),
           child: StyledButton(
               onPressed: () {
-                print("pressed");
                 !loggedIn ? context.push('/sign-in') : signOut();
               },
               child: !loggedIn ? const Text('Login') : const Text('Logout')),
