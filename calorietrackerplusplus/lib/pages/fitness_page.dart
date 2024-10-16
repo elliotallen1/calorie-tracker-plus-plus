3c4e4fd9fe7dc0be752bf8f4c5eaaede910376a3
elliotallen1 <allened@hendrix.edu>
2024-10-15 21:40:17 -0500

    Refactor

--- calorietrackerplusplus/lib/pages/fitness_page.dart
+++ calorietrackerplusplus/lib/pages/fitness_page.dart
@@ -1,5 +1,5 @@
-import 'package:calorietrackerplusplus/main.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:calorietrackerplusplus/log_calories_dialog.dart';
+import 'package:calorietrackerplusplus/set_goal_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider; 
 import 'package:provider/provider.dart';
@@ -8,9 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';    
 import 'package:calorietrackerplusplus/src/authentication.dart';
 import 'package:calorietrackerplusplus/src/widgets.dart';
-import 'friends_page.dart';
-
-
 
 class FitnessPage extends StatelessWidget {
   const FitnessPage({super.key});
@@ -33,115 +30,36 @@ class FitnessPage extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Expanded(child: Text("stuff")),
+          Expanded(child: const Text("stuff")),
           Consumer<ApplicationState>(
             builder: (context, appState, _) => Column(
               children: [
                 if (appState.loggedIn) ...[
-                  // Existing "Set Goal" button
                   StyledButton(
                     onPressed: () {
-                      _showSetGoalDialog(context, appState);
+                      showDialog(
+                        context: context,
+                        builder: (context) => SetGoalDialog(),
+                      );
                     },
                     child: const Text('Set Goal'),
                   ),
-                  // Existing "Friends" button
                   StyledButton(
                     onPressed: () => context.push('/friends'),
                     child: const Text('Friends'),
                   ),
-                  // New "Log Calories" button
                   StyledButton(
                     onPressed: () {
-                      _showLogCaloriesDialog(context);
+                      showDialog(
+                        context: context,
+                        builder: (context) => LogCaloriesDialog(),
+                      );
                     },
                     child: const Text('Log Calories'),
                   ),
                 ],
               ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Existing Set Goal dialog
-  void _showSetGoalDialog(BuildContext context, ApplicationState appState) {
-    final _goalController = TextEditingController();
-    
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Set Daily Calorie Goal'),
-        content: TextField(
-          controller: _goalController,
-          keyboardType: TextInputType.number,
-          decoration: const InputDecoration(hintText: 'Enter new calorie goal'),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () async {
-              if (_goalController.text.isNotEmpty) {
-                final newGoal = int.parse(_goalController.text);
-                await FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(FirebaseAuth.instance.currentUser!.uid)
-                      .set({'calorieGoal': newGoal}, SetOptions(merge: true));
-              }
-              Navigator.of(context).pop();
-            },
-            child: const Text('Save'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // New method to log calories
-  void _showLogCaloriesDialog(BuildContext context) {
-    final _calorieController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Log Calories Consumed'),
-        content: TextField(
-          controller: _calorieController,
-          keyboardType: TextInputType.number,
-          decoration: const InputDecoration(hintText: 'Enter calories'),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () async {
-              if (_calorieController.text.isNotEmpty) {
-                final newCalories = int.parse(_calorieController.text);
-                final userId = FirebaseAuth.instance.currentUser!.uid;
-                final today = DateTime.now().toIso8601String().split('T')[0]; // YYYY-MM-DD
-
-                final progressDoc = FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(userId)
-                    .collection('progress')
-                    .doc(today);
-
-                await FirebaseFirestore.instance.runTransaction((transaction) async {
-                  final snapshot = await transaction.get(progressDoc);
-                  if (snapshot.exists) {
-                    final currentCalories = snapshot.data()?['caloriesConsumed'] ?? 0;
-                    transaction.update(progressDoc, {
-                      'caloriesConsumed': currentCalories + newCalories,
-                    });
-                  } else {
-                    transaction.set(progressDoc, {
-                      'caloriesConsumed': newCalories,
-                    });
-                  }
-                });
-              }
-              Navigator.of(context).pop();
-            },
-            child: const Text('Save'),
           ),
         ],
       ),
