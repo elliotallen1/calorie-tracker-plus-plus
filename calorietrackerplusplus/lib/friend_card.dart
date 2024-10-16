b81408ceef7dd2905752fada589be89c502ef4d1
Amelia Jones <jonesas@hendrix.edu>
2024-10-16 11:58:37 -0500

    Changed shape of friend cards?

--- calorietrackerplusplus/lib/friend_card.dart
+++ calorietrackerplusplus/lib/friend_card.dart
@@ -14,8 +14,10 @@ class FriendCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      shape: StadiumBorder(side: BorderSide(color: Colors.black)),
+      margin: const EdgeInsets.symmetric(vertical: 8.0), 
+      //color: Color.fromARGB(255, 165, 244, 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0),
+      side: BorderSide(color: Colors.black)),
       child: Column(
           children: [
             Text(
